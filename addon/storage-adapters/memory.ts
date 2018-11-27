2827f360223721406494c4f7f6116933fdea56d5
Eugenio Mendoza Robaina <eugenio.mendoza2@bbva.com>
2018-11-27 12:50:46 +0100

    docs(readme): readme file

--- addon/storage-adapters/memory.ts
+++ addon/storage-adapters/memory.ts
@@ -3,7 +3,7 @@ import EmberObject from '@ember/object';
 
 export interface MemoryInterface extends StorageAdapterInterface {}
 
-export default class Memory extends EmberObject {
+export default class Memory extends EmberObject implements MemoryInterface {
 	private memory: Array<Object>;
 
 	constructor() {
